08f1ef337387419fe6519c2c06f320b33b72b4f2
francis <franrtg96@gmail.com>
2022-10-31 21:54:15 -0700

    added paypal buttons

--- components/NavBar/Navbar.jsx
+++ components/NavBar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import ButtonPiggy from '../ButtonPiggy'
+
 export default function Navbar() {
   return (
-    <nav className='navbar d-flex col-lg-12'>
-        <div className='d-flex col-lg-4 justify-content-center'>
-          <img src="/images/logotipo.svg" alt="" />
-        </div>
-        <div className='pigContainer'>
-          
-        </div>
+    <nav className='navbar d-flex col-lg-12 align-items-center'>
+      <div className='d-flex col-lg-7 justify-content-end align-items-center'>
+        <img src="/images/logotipo.svg" alt="" />
+      </div>
+      <div className='d-flex col-lg-5 justify-content-center align-items-center' >
+        <a href="https://www.paypal.com/donate/?hosted_button_id=3UR59FW8C2UGC">
+          <img className='pigContainer' src="/images/piggy.svg" alt="donate" />
+        </a>
+      </div>
     </nav>
   )
 }
