cf48a920273d84b010de5877e363e4c8ed82c433
disection <diseccionmexico@gmail.com>
2022-10-31 19:15:17 -0600

    ADD dependecies React Boostrap

---
+++ pages/componets.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from 'componets/button.modules.scss'
+
+export default function home() {
+  return (
+    <>
+    
+    
+    <div className='group-button'>1 B<span className='group-button__action'><i className="bi bi-plus"></i></span></div>
+    </>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function home() {
   return (
+    
     <div>home</div>
+    
   )
 }
