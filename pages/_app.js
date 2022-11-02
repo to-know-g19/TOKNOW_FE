62af89fb1be74f54edf4f2e6b8c7e8384096480a
francis <franrtg96@gmail.com>
2022-11-02 00:02:50 -0700

    added landing and components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import '../pages/home.scss'
+import '../components/CardInfoLanding/CardInfoLanding.scss'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
