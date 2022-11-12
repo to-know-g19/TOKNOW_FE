2e96d143b4ea536b0f5e2a67784afabbfa59448d
francis <franrtg96@gmail.com>
2022-11-11 22:49:21 -0700

    created formGroup component to post and GroupsDisplay to show groups

---
+++ components/CreateGroupsDisplay/CreateGroupsDisplay.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import GroupCircle from '../../components/GroupCircle/GroupCircle'
+
+
+
+// const grupos = [
+//     {
+//         grade:"1",
+//         group:"Z"
+//     },
+//     {
+//         grade:"1",
+//         group:"Z"
+//     },
+// ]
+
+export default function CreateGroupsDisplay() {
+    const [grupos, setGrupos] = useState([])
+    // const token = localStorage.getItem('token')
+    useEffect(() => {
+        fetch('https://api.2know.today/group',{
+        mode: 'cors',
+        headers: {
+            'Content-type': 'application/json',
+            // "Authorization": `Bearer ${token}`
+        },
+        // body: JSON.stringify(group)
+        
+        }
+            )
+         .then((response) => response.json())
+         .then(data => {
+            setGrupos(data.data.groups)
+            console.log("Grupos: ", data)
+         })
+    }, [])
+
+    // const grupos = fetch('https://api.2know.today/group',{
+    // })
+
+
+    return (
+        <>
+
+                <div className='group-Wrapper d-flex'>
+                    <div className='groups-container d-flex'>
+                        {grupos.map(grupo => {
+                            return <GroupCircle grade={grupo.grade} group={grupo.name} />
+                        })}
+
+
+                    </div>
+                </div>
+        </>
+    )
+}
