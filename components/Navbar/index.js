311b22063213dee9b1c6a6485b5edb92384c55be
francis <franrtg96@gmail.com>
2022-11-17 20:17:56 -0700

    nav and register button have link

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -12,15 +12,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Navbar() {
     return (
-        <nav className="navigation d-flex col-lg-12 justify-content-around">
+        <div className="navigation d-flex col-lg-12 justify-content-center">
+        <nav className=" d-flex col-lg-11 justify-content-between">
             <div className="d-flex  align-items-center">
                 <a href=""><i className="bi bi-list"></i></a>
-                <Image src={logo} alt="Logo toKnow" />
+               <Link href="/"> <Image src={logo} alt="Logo toKnow" /> </Link>
             </div>
             <div className="d-flex  justify-content-center align-items-center">
                 <div><a href=""><i className="bi bi-bell"><span> <i className="bi bi-plus"></i></span></i></a></div>
                 <Link href="" label='user'>user</Link>
             </div>
         </nav>
+        </div>
     )
 }
