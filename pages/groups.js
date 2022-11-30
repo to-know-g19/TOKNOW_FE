f1404ee258449511aa141204b5c74afb8c2ba780
francis <franrtg96@gmail.com>
2022-11-29 21:58:29 -0700

    functional group modal

--- pages/groups.js
+++ pages/groups.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import GroupCardDisplay from '../components/GroupCardDisplay/GroupCardDisplay'
 import Layout from '../components/Layout'
+import GroupModal from '../components/GroupModal/GroupModal'
 
 export default function groups() {
   return (
     <Layout>
     <div>
+      <div>hola</div>
+      <div><GroupModal/></div>
         <GroupCardDisplay/>
     </div>
     </Layout>
