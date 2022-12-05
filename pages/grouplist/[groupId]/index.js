3ac564571b9c2b3a713c73c2db10ff56419d3752
francis <franrtg96@gmail.com>
2022-12-04 19:51:02 -0700

    groups are fetched by id

--- pages/grouplist/[groupId]/index.js
+++ pages/grouplist/[groupId]/index.js
@@ -1,29 +1,54 @@
 /* default */
-import  { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Layout from '../../../components/Layout'
 
-function GroupDetail(){
+export default function GroupDetail() {
     const router = useRouter()
     const groupId = router.query.groupId
-    return(
-       <>
-        <Link> Detalle del grupo {groupId} </Link>
-        <h2>materias</h2>
-        <ul>
-            <li>materias 1</li>            
-        </ul>
-        <h2>students</h2>
-        <ul>
-            <li>materias 1</li>            
-        </ul>
-        <h2>Teacher</h2>
-        <ul>
-            <li>materias 1</li>            
-        </ul>
+    const [group, setGroups] = useState([])
+
+    useEffect(() => {
+       
+        const token = localStorage.getItem('token')
+        fetch(`https://api.2know.today/group/${groupId}`, {
+            mode: 'cors',
+            headers: {
+                'Content-type': 'application/json',
+                "Authorization": `Bearer ${token}`
+            },
+        }
+        )
+            .then((response) => response.json())
+            .then(data => {
+                console.log("soy data", data)
+          
+                setGroups(data.data)
+                // console.log(pathName)
+                //  console.log("GRUPOS: ", group)
+            })
+    }, [router.query])
+
+    return (
+        <>
+            <Layout>
+                {/* <Link> Detalle del grupo {groupId} </Link> */}
 
 
+                <h2>materias</h2>
+                <ul>
+                    <li>materias 1</li>
+                </ul>
+                <h2>students</h2>
+                <ul>
+                    <li>materias 1</li>
+                </ul>
+                <h2>Teacher</h2>
+                <ul>
+                    <li>materias 1</li>
+                </ul>
+
+            </Layout>
         </>
-
-        
     )
 }
-export default GroupDetail
