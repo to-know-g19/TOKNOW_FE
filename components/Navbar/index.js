85065090020f7f9634d350bc0cb9d67edfed16a5
francis <franrtg96@gmail.com>
2023-01-05 19:51:45 -0700

    started to display close icon in nav (not functional nor animated)

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -5,30 +5,39 @@ import Link from 'next/link'
 import logo from '../../public/img/logos/logo.svg'
 /* styles */
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React, { useState } from "react";
+import { IoIosBackspace } from "react-icons/io";
 
 
 
 export default function Navbar() {
+    const [sidebar, setSidebar] = useState(false)
+    const showSidebar = () => setSidebar(!sidebar)
+
     return (
         <div className="navigation d-flex col-lg-12 ">
 
             <div className="d-flex col-lg-2 justify-content-center  align-items-center">
                 <div className="navigation__navmenuBG d-flex justify-content-center  align-items-center">
-                    <a href=""><i className=" bi bi-list"></i></a>
+                    <a onClick={showSidebar} href="#">
+                        <i className=" bi bi-list"></i>
+                    </a>
                 </div>
-            </div>
-            <div className="d-flex col-lg-8 justify-content-center align-items-center">
-                <Link href="/"> <Image src={logo} alt="Logo toKnow" /> </Link>
             </div>
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className="nav-menu-items">
                     <li className="navbar-toggle">
-                        <Link to='#'></Link>
+                        <Link href="/" className="menu-bars">
+                            <IoIosBackspace />
+                        </Link>
                     </li>
                 </ul>
-
             </nav>
+            {/* LOGO ToKnow */}
+            <div className="d-flex col-lg-8 justify-content-center align-items-center">
+                <Link href="/"> <Image src={logo} alt="Logo toKnow" /> </Link>
+            </div>
+            
 
             {/* <div className="d-flex  justify-content-center align-items-center">
                     <div><a href=""><i className="bi bi-bell"><span> <i className="bi bi-plus"></i></span></i></a></div>
