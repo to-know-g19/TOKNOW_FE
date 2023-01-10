c2c2bc3482c345805a8f22dc66b21a11d4d5bf24
francis <franrtg96@gmail.com>
2023-01-09 17:23:07 -0700

    sidebar hides and shows on click

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -6,7 +6,7 @@ import logo from '../../public/img/logos/logo.svg'
 /* styles */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from "react";
-import { IoIosBackspace } from "react-icons/io";
+import { SlClose } from "react-icons/sl";
 import { SidebarData } from "../SideBar/SidebarData";
 
 
@@ -16,42 +16,44 @@ export default function Navbar() {
 
     return (
         // removí el primer d-flex para pruebas
-        <div className="navigation col-lg-12 ">
+        <div className="navigation d-flex col-lg-12 ">
 
             <div className="d-flex col-lg-2 justify-content-center  align-items-center">
                 <div className="navigation__navmenuBG d-flex justify-content-center  align-items-center">
-                    <a onClick={showSidebar} href="#">
+                    <Link onClick={showSidebar} href="#">
                         <i className=" bi bi-list"></i>
-                    </a>
+                    </Link>
                 </div>
             </div>
-            
-            
+
+            {/* SIDEBAR */}
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'} >
-                <ul className="nav-menu-items">
+                <ul className="nav-menu-items" onClick={showSidebar}>
                     <li className="navbar-toggle">
                         <Link href="/" className="menu-bars">
-                            <IoIosBackspace />
+                            <SlClose />
                         </Link>
                     </li>
+
                     {SidebarData.map((item, index) => {
                         return (
                             <li key={index} className={item.className}>
                                 <Link href={item.path}>
                                     {item.icon}
-                                    <span>{item.title}</span>
+                                    <span className="nav-span">{item.title}</span>
                                 </Link>
                             </li>
                         )
                     })}
+                    
                 </ul>
-            </nav> 
-            
+            </nav>
+
             {/* LOGO ToKnow */}
             <div className="d-flex col-lg-8 justify-content-center align-items-center">
                 <Link href="/"> <Image src={logo} alt="Logo toKnow" /> </Link>
             </div>
-            
+
 
             {/* <div className="d-flex  justify-content-center align-items-center">
                     <div><a href=""><i className="bi bi-bell"><span> <i className="bi bi-plus"></i></span></i></a></div>
