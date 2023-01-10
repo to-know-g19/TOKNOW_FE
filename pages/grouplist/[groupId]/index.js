fd2fa00fa6b83a65210f508dc71db500ad2a4378
francis <franrtg96@gmail.com>
2023-01-10 15:54:04 -0700

    added icons on top of teachers that will act as buttons to add a new teacher

--- pages/grouplist/[groupId]/index.js
+++ pages/grouplist/[groupId]/index.js
@@ -3,6 +3,9 @@ import { useRouter } from 'next/router'
 import Layout from '../../../components/Layout'
 import TeacherRectangle from '../../../components/teacherRectangle/TeacherRectangle'
 import Link from 'next/link'
+/* icon imports*/
+import { FaUserCircle } from 'react-icons/fa';
+import { AiFillPlusCircle } from 'react-icons/ai';
 
 export default function GroupDetail() {
     const router = useRouter()
@@ -43,14 +46,20 @@ export default function GroupDetail() {
                 {/* <Link> Detalle del grupo {groupId} </Link> */}
                 <div className='d-flex'>
                     <div className='d-flex flex-column col-lg-6 align-items-center'>
-                        <div className='d-flex col-lg-8'>
-                            <h4>Profesores</h4>
+                        <div className='d-flex col-lg-9' >
+                            <div className='d-flex col-lg-6 justify-content-between'>
+                                <div className='d-flex col-lg-9 justify-content-between'>
+                                    <h4><FaUserCircle /></h4>
+                                    <h4>Profesores</h4>
+                                </div>
+                                <Link><h4><AiFillPlusCircle /></h4></Link>
+                            </div>
                         </div>
                         {(Array.isArray(teachers) && teachers.length > 0) ?
                             teachers.map((teacher) => {
                                 return (
-                                    <Link href="/grouplist/[groupId]/teacher/[teacherId]" 
-                                    as={`/grouplist/${groupId}/teacher/${teacher._id}`} key={teacher._id} >
+                                    <Link href="/grouplist/[groupId]/teacher/[teacherId]"
+                                        as={`/grouplist/${groupId}/teacher/${teacher._id}`} key={teacher._id} >
                                         <TeacherRectangle
                                             key={teacher._id}
                                             teacher={teacher}
@@ -67,8 +76,8 @@ export default function GroupDetail() {
                         {(Array.isArray(students) && students.length > 0) ?
                             students.map(student => {
                                 return (
-                                    <Link href="/grouplist/[groupId]/student/[studentId]" 
-                                    as={`/grouplist/${groupId}/student/${student._id}`} key={student._id} >
+                                    <Link href="/grouplist/[groupId]/student/[studentId]"
+                                        as={`/grouplist/${groupId}/student/${student._id}`} key={student._id} >
                                         <TeacherRectangle
                                             key={student._id}
                                             teacher={student} />
