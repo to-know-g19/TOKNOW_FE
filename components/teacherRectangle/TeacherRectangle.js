a7cf8fded71dcc9e03a9c9b074a0c70fd634b8e0
francis <franrtg96@gmail.com>
2023-01-15 02:26:48 -0700

    added eye and trash icons with styles in TeacherRectangle

--- components/teacherRectangle/TeacherRectangle.js
+++ components/teacherRectangle/TeacherRectangle.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FaUserCircle } from 'react-icons/fa';
+import { FaRegEye } from 'react-icons/fa';
+import { BsFillTrashFill } from 'react-icons/bs';
 
 export default function TeacherRectangle({ teacher }) {
 
@@ -11,11 +13,15 @@ export default function TeacherRectangle({ teacher }) {
         <div className='d-flex col-lg-2 justify-content-center align-items-center'>
           <FaUserCircle className='teacherInfo__userCircle'/>
         </div>
-        <div className='col-lg-10'>
+        <div className='col-lg-8'>
           <div>
             <span>{` ${name} ${lastNameA} ${lastNameB} `}</span>
           </div>
           <span>{tipoProfesor}</span>
+        </div>
+        <div className='teacherInfo__icons d-flex col-lg-2 align-items-center justify-content-around'>
+          <FaRegEye />
+          <BsFillTrashFill />
         </div>
       </div>
     </div>
