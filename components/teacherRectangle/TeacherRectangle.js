0a2060353b0e11c7562ea84c32871d30e4e33953
francis <franrtg96@gmail.com>
2023-01-15 02:14:08 -0700

    added userIcon, classes and changed styles to TeacherRectangle

--- components/teacherRectangle/TeacherRectangle.js
+++ components/teacherRectangle/TeacherRectangle.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import { FaUserCircle } from 'react-icons/fa';
 
-export default function TeacherRectangle({teacher}){
+export default function TeacherRectangle({ teacher }) {
 
-  const {name, lastNameA, lastNameB, tipoProfesor} = teacher 
+  const { name, lastNameA, lastNameB, tipoProfesor } = teacher
   console.log("tipo de teacher", tipoProfesor)
   return (
     <div className='teacherInfo'>
-      <div className='teacherInfo__wrapper d-flex'>
-        
-        <div>
-          <span>{` ${name} ${lastNameA} ${lastNameB} `}</span>
+      <div className='teacherInfo__wrapper d-flex col-lg-12'>
+        <div className='d-flex col-lg-2 justify-content-center align-items-center'>
+          <FaUserCircle className='teacherInfo__userCircle'/>
         </div>
-        <span>{tipoProfesor}</span>
-
+        <div className='col-lg-10'>
+          <div>
+            <span>{` ${name} ${lastNameA} ${lastNameB} `}</span>
+          </div>
+          <span>{tipoProfesor}</span>
+        </div>
       </div>
     </div>
   )
