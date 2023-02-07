3ed937d2286e631603db0a63b08dc69877e5f8dd
francis <franrtg96@gmail.com>
2023-02-07 15:17:42 -0700

    changed api route from groups to school

--- components/GroupsDisplay/GroupsDisplay.js
+++ components/GroupsDisplay/GroupsDisplay.js
@@ -3,36 +3,27 @@ import GroupCircle from '../GroupCircle/GroupCircle'
 
 export default function GroupsDisplay() {
 
-    const [grupos, setGrupos] = useState([])
-    // useEffect(() =>{
-    //     const token = localStorage.getItem('token')
-    // }, [])
-    
+    const [grupos, setGrupos] = useState([]);
 
     useEffect(() => {
-        const token = localStorage.getItem('token')
-        fetch('https://api.toknow.online/group',{
-        mode: 'cors',
-        headers: {
-            'Content-type': 'application/json',
-            "Authorization": `Bearer ${token}`
-        }, }
-            )
-         .then((response) => response.json())
-         .then(data => {
-            setGrupos(data.data.groups)
-            //da error cuando se trata de entrar y no hay token en localStorage
-            console.log("Grupos: ", data.data.groups)
-         })
-    }, [])
+        const token = localStorage.getItem("token");
+    
+        fetch(`https://api.toknow.online/school`, {
+            mode: "cors",
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("soy data", data);
+                const schools = data.data.schools;
+                console.log("soy schools", schools);
 
-    // useEffect(async() =>{
-    //     const groups = await fetch ('https://api.2know.today/group',{
-
-    //     })
-    // })
- 
-
+            });
+    }, []);
+    
 
     return (
         <>
@@ -40,7 +31,7 @@ export default function GroupsDisplay() {
                 <div className='group-Wrapper d-flex'>
                     <div className='groups-container d-flex justify-content-center'>
                         {grupos.map(grupo => {
-                            return <GroupCircle grade={grupo.grade} group={grupo.name} />
+                            return <GroupCircle key={grupo._id} grade={grupo.grade} group={grupo.name} />
                         })}
 
 
