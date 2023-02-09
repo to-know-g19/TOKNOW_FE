5abe0f8dd8c135e4a763f5dc539f36e1f106df80
francis <franrtg96@gmail.com>
2023-02-09 02:31:37 -0700

    added function handleEyeClick in GroupCardDisplay to work with prop onEyeClick in GroupCard

--- components/GroupCardDisplay/GroupCardDisplay.js
+++ components/GroupCardDisplay/GroupCardDisplay.js
@@ -2,40 +2,45 @@ import React, { useEffect, useState } from 'react'
 import GroupCard from '../GroupCard/GroupCard'
 import Link from 'next/link'
 import ModalExample from '../GroupModal/GroupModal'
-import { BsArrowLeftCircle } from 'react-icons/bs';
-import { FiArrowLeftCircle } from 'react-icons/fi';
-import { HiOutlineArrowCircleLeft } from 'react-icons/hi';
 import ArrowGoBack from '../ArrowGoBack/ArrowGoBack';
+import { useRouter } from 'next/router';
 
 export default function GroupCardDisplay() {
   const [grupos, setGrupos] = useState([]);
+  const router = useRouter()
 
   useEffect(() => {
-      const token = localStorage.getItem("token");
-      const userData = JSON.parse(atob(token.split(".")[1]));
-      const userId = userData.id;
-      console.log('user id', userId)
-  
-      fetch(`https://api.toknow.online/school`, {
-          mode: "cors",
-          headers: {
-              "Content-type": "application/json",
-              "Authorization": `Bearer ${token}`
+    const token = localStorage.getItem("token");
+    const userData = JSON.parse(atob(token.split(".")[1]));
+    const userId = userData.id;
+    console.log('user id', userId)
+
+    fetch(`https://api.toknow.online/school`, {
+      mode: "cors",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("soy data", data);
+        const schools = data.data.schools;
+        console.log("soy schools", schools);
+        schools.forEach(school => {
+          if (school.user._id === userId) {
+            setGrupos(school.groups);
+            console.log("Grupos: ", school.groups);
           }
-      })
-          .then(response => response.json())
-          .then(data => {
-              console.log("soy data", data);
-              const schools = data.data.schools;
-              console.log("soy schools", schools);
-              schools.forEach(school => {
-                  if (school.user._id === userId) {
-                      setGrupos(school.groups);
-                      console.log("Grupos: ", school.groups);
-                  }
-              });
-          });
+        });
+      });
   }, []);
+
+  const handleEyeClick = (id) => {
+    router.push('/grouplist/' + id)
+  };
+
+
 
 
   return (
@@ -46,8 +51,8 @@ export default function GroupCardDisplay() {
         <div className='d-flex col-lg-12 justify-content-around'>
 
           <ArrowGoBack
-          btnTxtModal={<ModalExample/>}
-          route={'/registergroup'}/>
+            btnTxtModal={<ModalExample />}
+            route={'/registergroup'} />
 
         </div>
 
@@ -55,9 +60,10 @@ export default function GroupCardDisplay() {
         <div className='d-flex col-lg-10 flex-wrap justify-content-around'>
           {grupos.map(grupo => (
             //removí el return reemplazando las llaves despues de la flecha con parentesis
-            <Link className='col-lg-5' href={'/grouplist/' + grupo._id} key={grupo._id} style={{ textDecoration: 'none' }} >
-              <GroupCard grade={grupo.grade} group={grupo.name} />
-            </Link>
+            // <Link className='col-lg-5' href={'/grouplist/' + grupo._id} key={grupo._id} style={{ textDecoration: 'none' }} >
+            <div className='col-lg-5'>
+              <GroupCard key={grupo._id} grade={grupo.grade} group={grupo.name} onEyeClick={() => handleEyeClick(grupo._id)} />
+            </div>
           ))}
 
         </div>
