ad6e5831c235fae2fefae0077673adee0e046b7d
francis <franrtg96@gmail.com>
2023-02-22 11:19:44 -0700

    put radio buttons under password field with a title and a display flex

--- components/FormLogin/FormLogin.js
+++ components/FormLogin/FormLogin.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form"
 import 'bootstrap/dist/css/bootstrap.css'
 import { useRouter } from 'next/router'
@@ -38,89 +38,58 @@ export default function FormLogin() {
     //poner en local storage 
     localStorage.setItem("token", token)
     //condición para evitar el error al desencriptar token:"undefined"
-    if (token === undefined){
+    if (token === undefined) {
       window.alert("las credenciales introducidas son incorrectas")
     } else {
       //desencriptando el token para acceder a su info
-    const userData = JSON.parse(atob(token.split(".")[1]));
-    const userId = userData.id;
-    const userRole = userData.role;
-    console.log('soy id en tkn', userId)
-    console.log('soy rol en tkn', userRole)
+      const userData = JSON.parse(atob(token.split(".")[1]));
+      const userId = userData.id;
+      const userRole = userData.role;
+      console.log('soy id en tkn', userId)
+      console.log('soy rol en tkn', userRole)
 
-    // Fetch de users para filtrar con el mail que haga match
-    let usersResult = await fetch(`https://api.toknow.online/user`, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      },
-    })
-    const usersData = await usersResult.json()
-    console.log("users data", usersData)
+      // Fetch de users para filtrar con el mail que haga match
+      let usersResult = await fetch(`https://api.toknow.online/user`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      })
+      const usersData = await usersResult.json()
+      console.log("users data", usersData)
 
-    // Check if user has school data
-    const user = usersData.data.userAll.find(user => user.email === data.email);
-    if (!token) {
-      window.alert('las credenciales introducidas son incorrectas')
-    }
-    else{
-      if(userRole === "teacher"){
-        router.push("/teacher/yourgroups")
-      }else {
-        if(userRole ==="parent"){
-          router.push("/parent/yourgroups")
+      // Check if user has school data
+      const user = usersData.data.userAll.find(user => user.email === data.email);
+      if (!token) {
+        window.alert('las credenciales introducidas son incorrectas')
+      }
+      else {
+        if (userRole === "teacher") {
+          router.push("/teacher/yourgroups")
         } else {
+          if (userRole === "parent") {
+            router.push("/parent/yourgroups")
+          } else {
             if (user && user.school) {
-             router.push("/grouplist");
+              router.push("/grouplist");
             } else {
-               router.push("/registerschool");
-              }
+              router.push("/registerschool");
+            }
           }
         }
-    }
- 
+      }
 
-  }}
+
+    }
+  }
   //necesito acceder al usuario por medio del correo al que estoy proporcionando para ver si tiene school.
 
   return (
     <div className='d-flex col-12 '>
       <form onSubmit={handleSubmit(onSubmit)} className='d-flex col-12 flex-column justify-content-center'>
 
-      <div>
-        <label>
-          <input
-            type="radio"
-            value="user"
-            checked={selectedRole === 'user'}
-            onChange={handleRoleChange}
-          />
-          Usuario
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type="radio"
-            value="teacher"
-            checked={selectedRole === 'teacher'}
-            onChange={handleRoleChange}
-          />
-          Maestro
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type="radio"
-            value="parent"
-            checked={selectedRole === 'parent'}
-            onChange={handleRoleChange}
-          />
-          Papá
-        </label>
-      </div>
+
 
         <div className='col-12 '>
           <div className='d-flex col-12 flex-column'>
@@ -136,7 +105,6 @@ export default function FormLogin() {
               <label>Correo</label>
             </div>
           </div>
-
           <div className='col-12 d-flex flex-row'>
             <div className='d-flex col-12 flex-column'>
               <div className="form-floating mb-3">
@@ -152,6 +120,43 @@ export default function FormLogin() {
               </div>
             </div>
           </div>
+          <h6 className='landingLogin'>Selecciona tu rol</h6>
+          <div className='d-flex justify-content-around'>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value="parent"
+                  checked={selectedRole === 'parent'}
+                  onChange={handleRoleChange}
+                />
+                Padre/Tutor
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value="teacher"
+                  checked={selectedRole === 'teacher'}
+                  onChange={handleRoleChange}
+                />
+                Maestro
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value="user"
+                  checked={selectedRole === 'user'}
+                  onChange={handleRoleChange}
+                />
+                Administrador
+              </label>
+            </div>
+          </div>
+
         </div>
         <div className='d-flex justify-content-around'>
           <button className='btn-form col-4' type='submit'>Ingresar</button>
