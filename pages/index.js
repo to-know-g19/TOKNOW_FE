aee69527aa6de75aced8d01704658a4882fb4bfb
francis <franrtg96@gmail.com>
2023-02-28 11:35:35 -0700

    text correction

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,15 @@ export default function home() {
         <section className='col-12 col-lg-6'>
           <h2 className=''>toKnow</h2>
           <h5 className='landingSubtitle'>Conectando a la comunidad escolar</h5>
-          <div className=''>¡Bienvenid@s a toKnow! 
-          Somo una divertida plataforma que te ayudará a conectar con la comunidad escolar de tus hijos y alumnos. 
-          En esta página encontrarás información útil sobre tu escuela, inclyendo detalles de grupos y sus profesores. </div>
+          <div className=''>¡Bienvenidos a toKnow!
+            Somos una plataforma que te ayudará a conectar con la comunidad escolar de tus hijos y alumnos.
+            En esta página encontrarás información útil sobre tu escuela, incluyendo detalles de grupos y
+            sus profesores. </div>
         </section>
 
         <section className='col-12 col-lg-4'>
           <h5 className='landingLogin'>Acceder</h5>
-          <FormLogin/> 
+          <FormLogin />
           <div className='d-flex justify-content-lg-between'>
             {/* <div className='col-lg-5'>sus datos están seguros con nosotros</div> */}
             <div className='landingButtons d-flex'>
@@ -39,37 +40,42 @@ export default function home() {
         </section>
       </article>
 
-      <article className='sectionTwoHome d-flex col-lg-12 justify-content-center'>
+      <article className='sectionTwoHome d-flex col-lg-12 justify-content-around'>
         <CardInfoLanding
           ImgSrc='/img/Frame1.png'
           Title='#1 Estamos a un click'
-          Description='Haz click en el botón de Registro y comienza a explorar todas las herramientas que tenemos para tí'/>
+          Description='Haz click en el botón de Registro y comienza a explorar todas las 
+          herramientas que tenemos para tí' />
 
         <CardInfoLanding
           ImgSrc='/img/Frame2.png'
           Title='#2 Accede a tu grupo'
-          Description='Acceder a los grupos de clase de tus hijos nunca fue tan fácil, tan solo ingresa a la plataforma y escoge el grupo que deseas entrar'/>
+          Description='Acceder a los grupos de clase de tus hijos nunca fue tan fácil, tan solo 
+          ingresa a la plataforma y escoge el grupo que deseas entrar' />
 
         <CardInfoLanding
           ImgSrc='/img/Frame3.png'
           Title='#3 Información a la mano'
-          Description='Con toKnow la información que necesitas está siempre a la mano, encuentra todo lo que necesitas saber de la comunidad escolar'/>
+          Description='Con toKnow la información que necesitas está siempre a la mano, encuentra 
+          todo lo que necesitas saber de la comunidad escolar' />
 
         <CardInfoLanding
           ImgSrc='/img/Frame4.png'
           Title='#4 El poder de estar conectado!'
-          Description='Descubre el poder de estar conectado con nuestra aplicación de escuela primaria, donde podrás ingresar fácilmente y estar al tanto de todo lo que sucede en tu escuela'/>
+          Description='Descubre el poder de nuestra aplicación, donde podrás ingresar fácilmente y 
+          estar al tanto de lo que sucede en tu escuela' />
       </article>
 
       <article className='sectionThreeHome d-flex col-lg-12 justify-content-end align-items-center'>
         <section className='col-lg-7'>
           <h3>Registrate para disfrutar del servicio</h3>
-          <div>¡Regístrate ahora mismo para disfrutar de todos los servicios que nuestra aplicación tiene para ofrecerte! Podrás crear tu escuela y hacer grupos con profesores y alumnos.</div>
+          <div>¡Regístrate ahora mismo para disfrutar de todos los servicios que nuestra aplicación tiene 
+            para ofrecerte! Podrás crear tu escuela y hacer grupos con profesores y alumnos.</div>
         </section>
         <section className='d-flex col-lg-4 justify-content-center align-items-center '>
-        <div className='d-flex justify-content-around'>
-          <Link className='col-12' href='/register'><button className='btn-form col-12'>Regístrate</button></Link>
-        </div>
+          <div className='d-flex justify-content-around'>
+            <Link className='col-12' href='/register'><button className='btn-form col-12'>Regístrate</button></Link>
+          </div>
         </section>
       </article>
 
@@ -78,7 +84,12 @@ export default function home() {
         <section className='landingTextNextToImg col-6'>
           <h3>Acompañando de la mano</h3>
           <div ><img className='landingLogoNextToImg' src="/img/logos/logo.svg" alt="" /></div>
-          <div>Nuestra aplicación de escuela digital te brinda la oportunidad de estar al tanto del progreso de tu hij@ en cualquier momento y lugar, simplemente ingresa a la plataforma y encuentra toda la información que necesitas, desde calificaciones y tareas, hasta notificaciones sobre eventos y actividades escolares. Sabemos que la educación de tus hijos es una prioridad, es por eso que hemos hecho que sea muy fácil y accesible para los padres el seguir acompañando a sus hijos en su proceso de aprendizaje. </div>
+          <div>Nuestra aplicación de escuela digital te brinda la oportunidad de estar al tanto
+            del progreso de tu hija o hijo en cualquier momento y lugar, simplemente ingresa a la plataforma
+            y encuentra toda la información que necesitas, desde calificaciones y tareas, hasta
+            notificaciones sobre eventos y actividades escolares. Sabemos que la educación de tus hijos
+            es una prioridad, es por eso que hemos hecho que sea muy fácil y accesible para los padres 
+            el seguir acompañandolos en su proceso de aprendizaje. </div>
         </section>
       </article>
 
@@ -95,31 +106,44 @@ export default function home() {
       </article>
 
       <article className='sectionSevenHome d-lg-flex col-lg-12'>
-        <div className='FAQ col-lg-6'>FAQ #1 ¿Cómo puedo acceder a la aplicación de toKnow?
+        <div className='FAQ col-lg-6'>¿Cómo puedo acceder a la aplicación de toKnow?
           <div>
-          Acceder a nuestra aplicación es muy fácil. Lo primero que debes hacer es ingresar a la página web desde tu navegador. Luego, crea tu cuenta de usuario utilizando tus datos personales y la información de contacto que te solicitemos. Una vez que hayas creado tu cuenta, podrás ingresar fácilmente a la plataforma y comenzar a disfrutar de todos los beneficios.
+            Acceder a nuestra aplicación es muy fácil. Lo primero que debes hacer es ingresar 
+            a la página web desde tu navegador. Luego, crea tu cuenta de usuario utilizando 
+            tus datos personales y la información de contacto que te solicitemos. Una vez 
+            que hayas creado tu cuenta, podrás ingresar fácilmente a la plataforma y comenzar 
+            a disfrutar de todos los beneficios.
           </div>
         </div>
-        <div className='FAQ col-lg-6'>FAQ #2 Qué debo hacer?
+        <div className='FAQ col-lg-6'>¿Qué debo hacer?
           <div>
-          Si tienes alguna duda o problema con el proceso de registro, nuestro equipo de soporte técnico estará a tu disposición para ayudarte en todo lo que necesites. ¡Anímate a ingresar ahora y descubre todas las posibilidades que nuestra aplicación de escuela digital tiene para ofrecerte!
+            Si tienes alguna duda o problema con el proceso de registro, nuestro equipo de 
+            soporte técnico estará a tu disposición para ayudarte en todo lo que necesites. 
+            ¡Anímate a ingresar ahora y descubre todas las posibilidades que nuestra aplicación 
+            de escuela digital tiene para ofrecerte!
           </div>
         </div>
-        <div className='FAQ col-lg-6'>FAQ #3 ¿Cómo puedo ver los grupos de mi hij@?
+        <div className='FAQ col-lg-6'>¿Cómo puedo ver los grupos de mi hijo?
           <div>
-          Para ver los grupos en los que está inscrito tu hijo primero debes ingresar con tu cuenta de usuario y seleccionar el grupo correspondiente al grado que está cursando. Luego, encontrarás una sección de Profesores y otra de Alumnos donde podrás visualizar la información que necesitas.
+            Para ver los grupos en los que está inscrito tu hijo primero debes ingresar con tu 
+            cuenta de usuario y seleccionar el grupo correspondiente al grado que está cursando. 
+            Luego, encontrarás una sección de Profesores y otra de Alumnos donde podrás visualizar 
+            la información que necesitas.
           </div>
         </div>
-        <div className='FAQ col-lg-6'>FAQ #4 ¿La aplicación de toKnow tiene algún costo?
+        <div className='FAQ col-lg-6'>¿La aplicación de toKnow tiene algún costo?
           <div>
-          No, nuestra aplicación es completamente gratuita para todas las escuelas, padres, estudiantes y profesores. No requerimos ningún pago para descargar o utilizar nuestra plataforma, ya que nuestro objetivo es hacer que la educación sea más accesible y fácil para todos.
+            No, nuestra aplicación es completamente gratuita para todas las escuelas, padres, 
+            estudiantes y profesores. No requerimos ningún pago para utilizar nuestra plataforma, 
+            ya que nuestro objetivo es hacer que la educación sea más accesible y fácil 
+            para todos.
           </div>
         </div>
       </article>
-      
+
     </Layout>
-    
-    
+
+
   )
 }
 
