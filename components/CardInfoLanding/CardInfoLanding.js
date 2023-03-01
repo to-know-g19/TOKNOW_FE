9c7b997c49c506f72f4e8c82308e8ea5213b7944
francis <franrtg96@gmail.com>
2023-02-28 22:29:39 -0700

    added padding to CardInfo (info with #1, #2, #3, #4)

--- components/CardInfoLanding/CardInfoLanding.js
+++ components/CardInfoLanding/CardInfoLanding.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 export default function CardInfoLanding({ImgSrc, Title, Description}) {
     return (
-        <div className='col-lg-2 d-flex flex-column justify-content-center  align-items-center'>
+        <div className='pt-2 pb-3 col-lg-2 d-flex flex-column justify-content-center  align-items-center'>
             <img className='cardImg' src={ImgSrc} alt="" />
             <h3 className='cardTitle'>{Title}</h3>
             <div className='cardDescription'>{Description}</div>
