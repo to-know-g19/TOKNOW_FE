6efe949e137b8193273a0b7191667273724e7a5c
francis <franrtg96@gmail.com>
2023-02-28 22:25:35 -0700

    centered button registra escuela in mobile

--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,16 @@ export default function home() {
 
     <Layout title='toKnow de la mano con tu hijo' description='Aplicación para escuelas, herramienta para desarrollar una comunicación efectiva en la comunidad'>
       <article className='sectionOneHome d-lg-flex justify-content-lg-around align-items-lg-center'>
-        <section className='flex-direction-column col-12 col-lg-6 align-items-end'>
+        <section className='flex-direction-column col-12 col-lg-6 align-items-end '>
           <h2 className='landingTitle'>toKnow</h2>
           <h5 className='landingSubtitle'>Conectando a la comunidad escolar</h5>
           <div className=''>¡Bienvenidos a toKnow!
             Somos una plataforma que te ayudará a conectar con la comunidad escolar de tus hijos y alumnos.
             En esta página encontrarás información útil sobre tu escuela, incluyendo detalles de grupos y
             sus profesores. </div>
-            <Link href='/register'><button className='btn-form col-6 bg-success'>Regístra tu escuela</button></Link>
+          <Link className='links col-12 d-flex justify-content-lg-start justify-content-center' href='/register'>
+            <button className='btn-form col-6 bg-success'>Regístra tu escuela</button>
+          </Link>
         </section>
 
         <section className='col-12 col-lg-4'>
@@ -70,7 +72,7 @@ export default function home() {
       <article className='sectionThreeHome d-lg-flex col-lg-12 justify-content-end align-items-center'>
         <section className='col-lg-7'>
           <h3>Registrate para disfrutar del servicio</h3>
-          <div>¡Regístrate ahora mismo para disfrutar de todos los servicios que nuestra aplicación tiene 
+          <div>¡Regístrate ahora mismo para disfrutar de todos los servicios que nuestra aplicación tiene
             para ofrecerte! Podrás crear tu escuela y hacer grupos con profesores y alumnos.</div>
         </section>
         <section className='d-flex col-lg-4 justify-content-center align-items-center '>
@@ -89,7 +91,7 @@ export default function home() {
             del progreso de tu hija o hijo en cualquier momento y lugar, simplemente ingresa a la plataforma
             y encuentra toda la información que necesitas, desde calificaciones y tareas, hasta
             notificaciones sobre eventos y actividades escolares. Sabemos que la educación de tus hijos
-            es una prioridad, es por eso que hemos hecho que sea muy fácil y accesible para los padres 
+            es una prioridad, es por eso que hemos hecho que sea muy fácil y accesible para los padres
             el seguir acompañandolos en su proceso de aprendizaje. </div>
         </section>
       </article>
@@ -108,43 +110,43 @@ export default function home() {
 
       <article className='sectionSevenHome d-lg-flex col-lg-12'>
         <div className='FAQ col-lg-6'>
-          <div className= 'FAQ__title'>¿Cómo puedo acceder a la aplicación de toKnow?</div>
+          <div className='FAQ__title'>¿Cómo puedo acceder a la aplicación de toKnow?</div>
           <div>
-            Acceder a nuestra aplicación es muy fácil. Lo primero que debes hacer es ingresar 
-            a la página web desde tu navegador. Luego, crea tu cuenta de usuario utilizando 
-            tus datos personales y la información de contacto que te solicitemos. Una vez 
-            que hayas creado tu cuenta, podrás ingresar fácilmente a la plataforma y comenzar 
+            Acceder a nuestra aplicación es muy fácil. Lo primero que debes hacer es ingresar
+            a la página web desde tu navegador. Luego, crea tu cuenta de usuario utilizando
+            tus datos personales y la información de contacto que te solicitemos. Una vez
+            que hayas creado tu cuenta, podrás ingresar fácilmente a la plataforma y comenzar
             a disfrutar de todos los beneficios.
           </div>
         </div>
         <div className='FAQ col-lg-6'>
-          <div className= 'FAQ__title'>¿Qué debo hacer?</div>
+          <div className='FAQ__title'>¿Qué debo hacer?</div>
           <div>
-            Si tienes alguna duda o problema con el proceso de registro, nuestro equipo de 
-            soporte técnico estará a tu disposición para ayudarte en todo lo que necesites. 
-            ¡Anímate a ingresar ahora y descubre todas las posibilidades que nuestra aplicación 
+            Si tienes alguna duda o problema con el proceso de registro, nuestro equipo de
+            soporte técnico estará a tu disposición para ayudarte en todo lo que necesites.
+            ¡Anímate a ingresar ahora y descubre todas las posibilidades que nuestra aplicación
             de escuela digital tiene para ofrecerte!
           </div>
         </div>
         <div className='FAQ col-lg-6'>
-          <div className= 'FAQ__title'>
-          ¿Cómo puedo ver los grupos de mi hijo?
+          <div className='FAQ__title'>
+            ¿Cómo puedo ver los grupos de mi hijo?
           </div>
           <div>
-            Para ver los grupos en los que está inscrito tu hijo primero debes ingresar con tu 
-            cuenta de usuario y seleccionar el grupo correspondiente al grado que está cursando. 
-            Luego, encontrarás una sección de Profesores y otra de Alumnos donde podrás visualizar 
+            Para ver los grupos en los que está inscrito tu hijo primero debes ingresar con tu
+            cuenta de usuario y seleccionar el grupo correspondiente al grado que está cursando.
+            Luego, encontrarás una sección de Profesores y otra de Alumnos donde podrás visualizar
             la información que necesitas.
           </div>
         </div>
         <div className='FAQ col-lg-6'>
-          <div className= 'FAQ__title'>
-          ¿La aplicación de toKnow tiene algún costo?
+          <div className='FAQ__title'>
+            ¿La aplicación de toKnow tiene algún costo?
           </div>
           <div>
-            No, nuestra aplicación es completamente gratuita para todas las escuelas, padres, 
-            estudiantes y profesores. No requerimos ningún pago para utilizar nuestra plataforma, 
-            ya que nuestro objetivo es hacer que la educación sea más accesible y fácil 
+            No, nuestra aplicación es completamente gratuita para todas las escuelas, padres,
+            estudiantes y profesores. No requerimos ningún pago para utilizar nuestra plataforma,
+            ya que nuestro objetivo es hacer que la educación sea más accesible y fácil
             para todos.
           </div>
         </div>
