e20a01f28b4bec6f6ae15c1e7e4e9b3dcf492c25
KikeBA <enrique.berdichevsky@gmail.com>
2023-03-01 13:00:30 -0600

    fix student-info for responsive display

--- components/StudentInfo/StudentInfo.js
+++ components/StudentInfo/StudentInfo.js
@@ -65,53 +65,61 @@ export default function StudentInfo({ student }) {
         <div className='d-flex justify-content-center col-12  '>
 
             <form className='d-flex mt-3 col-10 flex-column align-items-center justify-content-center'>
-                <div className='col-10 d-flex flex-wrap justify-content-around'>
+                <div className='col-10 d-flex flex-wrap justify-content-around align-items-center'>
 
-                    <div className='d-flex col-5 flex-column'>
-                        <div className="form-floating mb-3">
-                            <span
-                                name='name'
-                                className="form-control"
-                                placeholder='Nombre'
-                            >{name}</span>
+                    <div className='d-flex col-12 col-lg-5 flex-column justify-content-center align-items-center'>
 
-                            <label>Nombre</label>
+                        <div className='d-flex col-12 col-lg-12 flex-column'>
+                            <div className="form-floating mb-3">
+                                <span
+                                    name='name'
+                                    className="form-control"
+                                    placeholder='Nombre'
+                                >{name}</span>
+
+                                <label>Nombre</label>
+                            </div>
                         </div>
+
+                        <div className='d-flex col-12 col-lg-12 flex-column'>
+                            <div className="form-floating mb-3">
+                                <span
+                                    name='lastNameA'
+                                    className="form-control"
+                                    placeholder='Apellido Paterno'
+                                >{lastNameA}</span>
+
+                                <label>Apellido Paterno</label>
+                            </div>
+                        </div>
+
                     </div>
 
-                    <div className='d-flex col-5 flex-column'>
-                        <div className="form-floating mb-3">
-                            <span
-                                name='matricula'
-                                className="form-control"
-                                placeholder='Matrícula'
-                            >{matricula}</span>
-                            <label>Matrícula</label>
+                    <div className='d-flex col-12 col-lg-5 flex-column justify-content-center align-items-center'>
+
+                        <div className='d-flex col-12 col-lg-12 flex-column'>
+                            <div className="form-floating mb-3">
+                                <span
+                                    name='lastNameB'
+                                    className="form-control"
+                                    placeholder='Apellido Materno'
+                                >{lastNameB}</span>
+
+                                <label>Apellido Materno</label>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='d-flex col-5 flex-column'>
-                        <div className="form-floating mb-3">
-                            <span
-                                name='lastNameA'
-                                className="form-control"
-                                placeholder='Apellido Paterno'
-                            >{lastNameA}</span>
-
-                            <label>Apellido Paterno</label>
+                        <div className='d-flex col-12 col-lg-12 flex-column'>
+                            <div className="form-floating mb-3">
+                                <span
+                                    name='matricula'
+                                    className="form-control"
+                                    placeholder='Matrícula'
+                                >{matricula}</span>
+                                <label>Matrícula</label>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='d-flex col-5 flex-column'>
-                        <div className="form-floating mb-3">
-                            <span
-                                name='lastNameB'
-                                className="form-control"
-                                placeholder='Apellido Materno'
-                            >{lastNameB}</span>
-
-                            <label>Apellido Materno</label>
-                        </div>
                     </div>
 
 
@@ -130,7 +138,7 @@ export default function StudentInfo({ student }) {
                     {/* link al parent */}
 
                 </div>
-                <div className='d-flex align-items-center justify-content-center col-5 flex-column'>
+                <div className='d-flex align-items-center justify-content-center col-12 col-lg-5 flex-column'>
                     <h4>Datos del tutor</h4>
                     {(parents.length) ?
                         parents.map(parent => {
