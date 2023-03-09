da82a93864a04c9a68bd6716c44279833f17168c
francis <franrtg96@gmail.com>
2023-03-09 15:56:32 -0700

    added better spacing for user circle

--- components/CardAnnouncement/index.js
+++ components/CardAnnouncement/index.js
@@ -10,9 +10,10 @@ export default function Index() {
                 <div className='card-announce d-flex col-11 col-lg-7 bg-light'>
                     <div className='d-flex flex-column flex-lg-column col-12 col-lg-12'>
                         <div className='d-flex'>
-                            <div className='p-3 d-flex justify-content-center align-items-center '>
+                            <div className='p-2 pt-lg-4 d-flex justify-content-center align-items-center '>
                                 <FaUserCircle className='card-announce__user-img' />
                             </div>
+
                             <div className='d-flex flex-column '>
                                 <div className='pt-3'>
                                     Carlos Francisco Arteaga Peregrina
@@ -21,14 +22,14 @@ export default function Index() {
                                     09 de marzo 2023
                                 </div>
                             </div>
-                            <div className='d-flex p-3 '>
+
+                            <div className='d-flex pt-3 p-2 p-lg-3 '>
                                 Administrador
-                            </div>
+                            </div> 
                         </div>
+
                         <div className='p-3 p-lg-4'>
                             Suspensión de labores el día 20 de noviembre
-                            Nuestra aplicación de escuela digital te brinda la oportunidad de estar al tanto del progreso de tu hija o hijo en cualquier momento y lugar, simplemente ingresa a la plataforma y encuentra toda la información que necesitas, desde calificaciones y tareas, hasta notificaciones sobre eventos y actividades escolares. Sabemos que la educación de tus hijos es una prioridad, es por eso que hemos hecho que sea muy fácil y accesible para los padres el seguir acompañandolos en su proceso de aprendizaje.
-
                         </div>
                     </div>
                 </div>
