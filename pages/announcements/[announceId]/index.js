6546ae71b0cf3e775e8e9ec0969672db67b515cb
francis <franrtg96@gmail.com>
2023-03-14 04:40:00 -0700

    using id from router to get announcement id from api

--- pages/announcements/[announceId]/index.js
+++ pages/announcements/[announceId]/index.js
@@ -1,24 +1,51 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ArrowGoBack from '../../../components/ArrowGoBack/ArrowGoBack'
 import Layout from '../../../components/Layout'
 import PostAnnouncement from '../../../components/PostAnnouncement'
+import { useRouter } from 'next/router';
+
 
 export default function AnnouncementId() {
+    const router = useRouter()
+    const announceId = router.query.announceId
+    const [announceInfo, setAnnounceInfo] = useState({})
+
+    //petición a la api para setear anuncios
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+            fetch(`https://api.toknow.online/announcement/${announceId}`, {
+                mode: "cors",
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log("la data", data)
+                    if (data.data){
+                    const announcement = data.data.announcementById
+                    console.log("anuncio details", announcement)
+                    setAnnounceInfo(announcement)}
+                })
+        
+    }, []);
     return (
         <Layout>
             <div>
                 <div>
                     <ArrowGoBack
-                    //agregar botón para ir al formulario de creación
-                    btnTxtModal={<h4>Anuncio</h4>}
-                    route={"/announcements"}/>
+                        //agregar botón para ir al formulario de creación
+                        btnTxtModal={<h4>Anuncio</h4>}
+                        route={"/announcements"} />
                     <PostAnnouncement
                         coverimg={"/img/kid&parent.jpeg"}
-                        userName={"carlos Francisco Arteaga"}
-                        role={"admin"}
+                        userName={"announceInfo.user.name"}
+                        role={"announceInfo.user.role"}
                         date={"17 de noviembre"}
-                        announcementTitle={"¡Bienvenido A TOKNOW!"}
-                        textInfo={"Nuestra aplicación de escuela digital te brinda la oportunidad de estar al tanto del progreso de tu hija o hijo en cualquier momento y lugar, simplemente ingresa a la plataforma y encuentra toda la información que necesitas, desde calificaciones y tareas, hasta notificaciones sobre eventos y actividades escolares. Sabemos que la educación de tus hijos es una prioridad, es por eso que hemos hecho que sea muy fácil y accesible para los padres el seguir acompañandolos en su proceso de aprendizaje."}
+                        announcementTitle={"announceInfo.announcementTitle"}
+                        textInfo={"announceInfo.announcementText"}
                     />
                 </div>
             </div>
