56f3fb9cd2bd89308e11a9195cff3001be3898f9
francis <franrtg96@gmail.com>
2023-03-14 03:07:00 -0700

    announcement cards showing info from api

--- pages/announcements/index.js
+++ pages/announcements/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout'
 import CardAnnouncement from '../../components/CardAnnouncement'
 import ArrowGoBack from '../../components/ArrowGoBack/ArrowGoBack'
@@ -7,8 +7,10 @@ import { ToastContainer } from 'react-toastify'
 import useToastify from '../../components/useToastify'
 
 export default function Announcements() {
-
+    
+    const [announceInfo, setAnnounceInfo] = useState([])
     const notifySuccess = useToastify("success", "¡Anuncio creado con éxito!")
+    
     useEffect(() => {
         const notifAnnounceCreation = localStorage.getItem('notifAnnounceCreation')
 
@@ -18,6 +20,27 @@ export default function Announcements() {
         }
 
     }, [])
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+
+            fetch(`https://api.toknow.online/announcement`, {
+                mode: "cors",
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    const allAnnouncements = data.data.announcement
+                    console.log("anuncios", allAnnouncements)
+                    setAnnounceInfo(allAnnouncements)
+                })
+        }
+    }, []);
     return (
         <Layout>
             <div>
@@ -25,36 +48,20 @@ export default function Announcements() {
                     btnTxtModal={<h4>Tablero de anuncios</h4>}
                     route={""} />
 
-                <CardAnnouncement
-                    coverimg={"../img/Frame1.png"}
-                    userName={"carlos Francisco Arteaga"}
-                    role={"admin"}
-                    date={"17 de noviembre"}
-                    announcementTitle={"Suspensión de labores el día 20 de noviembre"} />
+                {announceInfo.map(announce => {
+                    return (
+                        <CardAnnouncement
+                            key={announce.key}
+                            coverimg={"/img/kid&parent.jpeg"}
+                            userName={announce.user.name}
+                            role={announce.user.role}
+                            date={"--fecha--"}
+                            announcementTitle={announce.announcementTitle} />)
+                })}
 
-                <CardAnnouncement
-                    coverimg={"../img/Frame1.png"}
-                    userName={"carlos Francisco Arteaga"}
-                    role={"admin"}
-                    date={"17 de noviembre"}
-                    announcementTitle={"Suspensión de labores el día 20 de noviembre"} />
-
-                <CardAnnouncement
-                    coverimg={"../img/Frame1.png"}
-                    userName={"carlos Francisco Arteaga"}
-                    role={"admin"}
-                    date={"17 de noviembre"}
-                    announcementTitle={"Suspensión de labores el día 20 de noviembre"} />
-                    
-                <CardAnnouncement
-                    coverimg={"../img/Frame1.png"}
-                    userName={"carlos Francisco Arteaga"}
-                    role={"admin"}
-                    date={"17 de noviembre"}
-                    announcementTitle={"Suspensión de labores el día 20 de noviembre"} />
 
             </div>
-            <ToastContainer/>
+            <ToastContainer />
         </Layout>
     )
 }
