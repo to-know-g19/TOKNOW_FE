50512ef31365c66e599ca24f1699b71cbf6b045b
francis <franrtg96@gmail.com>
2023-03-27 20:41:48 -0700

    created component with fetch to display group and grade

---
+++ components/GroupInfoCircle/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import GroupCircle from '../GroupCircle/GroupCircle'
+import { useRouter } from 'next/router'
+
+export default function GroupInfo() {
+    const [groupInfo, setGroupInfo] = useState({})
+    const router = useRouter()
+    useEffect(() => {
+
+        const groupId = router.query.groupId
+        if (!!groupId) {
+            const token = localStorage.getItem('token')
+            fetch(`https://api.toknow.online/group/${groupId}`, {
+                mode: 'cors',
+                headers: {
+                    'Content-type': 'application/json',
+                    "Authorization": `Bearer ${token}`
+                },
+            })
+                .then((response) => response.json())
+                .then(data => {
+                    if (data.data) {
+                        setGroupInfo(data.data.groupById)
+                    }
+
+                })
+        }
+    }, [router.query.groupId])
+    return (
+        <div>
+            <GroupCircle
+                grade={groupInfo.grade}
+                group={groupInfo.name}
+            />
+        </div>
+    )
+}
--- pages/grouplist/[groupId]/index.js
+++ pages/grouplist/[groupId]/index.js
@@ -12,6 +12,7 @@ import { AiFillPlusCircle } from 'react-icons/ai';
 import { ToastContainer } from 'react-toastify'
 import useToastify from '../../../components/useToastify'
 import GroupCircle from '../../../components/GroupCircle/GroupCircle'
+import GroupInfo from '../../../components/GroupInfoCircle'
 
 export default function GroupDetail() {
     const router = useRouter()
@@ -48,7 +49,6 @@ export default function GroupDetail() {
         const handleEyeClick = (groupId, userId, strRutaExtra) => {
             router.push(`/grouplist/${groupId}/${strRutaExtra}${userId}`)
         };
-        console.log(groupInfo)
 
     const handleTrashClick = (strRoute, userId) => {
         const token = localStorage.getItem("token");
@@ -116,9 +116,7 @@ export default function GroupDetail() {
             <Layout>
                 <div>
                     <ArrowGoBack
-                        btnTxtModal={<GroupCircle
-                            grade={groupInfo.grade}
-                            group={groupInfo.name}/>}
+                        btnTxtModal={<GroupInfo/>}
                         btnTxtModal2nd={
                             userRole !== "parent" ? (
                                 <Link href={'/grouplist/[groupId]/newgroupannouncement'} as={`/grouplist/${groupId}/newgroupannouncement`} >
