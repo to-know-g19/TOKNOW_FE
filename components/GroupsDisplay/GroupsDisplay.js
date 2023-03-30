52936ddadd76dbcef993c96b703cfc5419a9b4c3
francis <franrtg96@gmail.com>
2023-03-30 15:07:19 -0700

    added spacing to code in GroupsDisplay

--- components/GroupsDisplay/GroupsDisplay.js
+++ components/GroupsDisplay/GroupsDisplay.js
@@ -5,48 +5,48 @@ export default function GroupsDisplay() {
 
     const [grupos, setGrupos] = useState([]);
 
-    useEffect(() => {   
+    useEffect(() => {
         const token = localStorage.getItem("token");
-        if(token) {
-        const userData = JSON.parse(atob(token.split(".")[1]));
-        const userId = userData.id;
-        
-    
-        fetch(`https://api.toknow.online/school`, {
-            mode: "cors",
-            headers: {
-                "Content-type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
-        })
-            .then(response => response.json())
-            .then(data => {
-                
-                const schools = data.data.schools;
-                
-                schools.forEach(school => {
-                    if (school.user !== null && school.user._id === userId) {
-                        setGrupos(school.groups);
-                        
-                    }
+        if (token) {
+            const userData = JSON.parse(atob(token.split(".")[1]));
+            const userId = userData.id;
+
+
+            fetch(`https://api.toknow.online/school`, {
+                mode: "cors",
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+
+                    const schools = data.data.schools;
+
+                    schools.forEach(school => {
+                        if (school.user !== null && school.user._id === userId) {
+                            setGrupos(school.groups);
+
+                        }
+                    });
                 });
-            });
         }
     }, []);
-    
+
 
     return (
         <>
 
-                <div className='group-Wrapper d-flex justify-content-center flex-wrap'>
-                    <div className='groups-container d-flex col-lg-6 flex-wrap justify-content-center'>
-                        {grupos.map(grupo => {
-                            return <GroupCircle key={grupo._id} grade={grupo.grade} group={grupo.name} />
-                        })}
+            <div className='group-Wrapper d-flex justify-content-center flex-wrap'>
+                <div className='groups-container d-flex col-lg-6 flex-wrap justify-content-center'>
+                    {grupos.map(grupo => {
+                        return <GroupCircle key={grupo._id} grade={grupo.grade} group={grupo.name} />
+                    })}
 
 
-                    </div>
                 </div>
+            </div>
         </>
     )
 }
